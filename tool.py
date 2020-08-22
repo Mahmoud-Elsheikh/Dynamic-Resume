5a7f3bdd82c07e5b233323a3604b39d19bdb4f7f
Mahmoud <Mahmoudelsheikh@hotmail.co.uk>
2020-08-22 15:20:09 +0400

    input data now from external JSON

--- tool.py
+++ tool.py
@@ -1,9 +1,14 @@
+import json
 from jinja2 import Environment, FileSystemLoader
 if __name__ == "__main__":
+    with open('projects.json', 'r') as file:
+        data=file.read()
+    projects= json.loads(data)
     env = Environment(
         loader=FileSystemLoader('templates'),
     )
-    template = env.get_template('test.txt')
-    template.stream(name="Blank", day="Friday").dump("./output/output.txt")
+    #template.stream(name="Blank", day="Friday").dump("./output/output.txt")
+    latex= env.get_template('latex.txt')
+    print(latex.render(Projects=projects))
 
 
